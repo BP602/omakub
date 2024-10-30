ed464320f0f413776b84eab2bfc7f22323d77abe
BP602 <BP602@pontet.eu>
2024-10-30 23:31:02 +0100

    Use the official Charm reposition to install Gum

--- install/terminal/required/app-gum.sh
+++ install/terminal/required/app-gum.sh
@@ -1,7 +1,10 @@
 # Gum is used for the Omakub commands for tailoring Omakub after the initial install
-cd /tmp
-GUM_VERSION="0.14.5" # Use known good version
-wget -qO gum.rpm "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum-${GUM_VERSION}-1.x86_64.rpm"
-sudo rpm install ./gum.rpm
-rm gum.rpm
+echo '[charm]
+name=Charm
+baseurl=https://repo.charm.sh/yum/
+enabled=1
+gpgcheck=1
+gpgkey=https://repo.charm.sh/yum/gpg.key' | sudo tee /etc/yum.repos.d/charm.repo
+sudo rpm --import https://repo.charm.sh/yum/gpg.key
+sudo dnf install gum
 cd -
